360cbc5207d75250dc09ec038c4210e58c884b82
williamhsieh <wh31110@gmail.com>
2022-05-08 22:15:13 +0800

    [FEAT] which-key now uses desc automatically

--- vim/.config/nvim/lua/config/whichkey.lua
+++ vim/.config/nvim/lua/config/whichkey.lua
@@ -1,16 +1,10 @@
--- TODO: move all keymap to mappings: https://github.com/AstroNvim/AstroNvim/commit/87eada0666f3b5e1a387a7206df3591af2cd6136
 local which_key = require("which-key")
 
-which_key.setup {--{{{
+which_key.setup {
   plugins = {
-    marks = true, -- shows a list of your marks on ' and `
-    registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
     spelling = {
-      enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
-      suggestions = 20, -- how many suggestions should be shown in the list?
+      enabled = true,
     },
-    -- the presets plugin, adds help for a bunch of default keybindings in Neovim
-    -- No actual key bindings are created
     presets = {
       operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
       motions = false, -- adds help for motions
@@ -21,253 +15,30 @@ which_key.setup {--{{{
       g = true, -- bindings for prefixed with g
     },
   },
-  -- add operators that will trigger motion and text object completion
-  -- to enable all native operators, set the preset / operators plugin above
-  -- operators = { gc = "Comments" },
-  key_labels = {
-    -- override the label used to display some keys. It doesn't effect WK in any other way.
-    -- For example:
-    -- ["<space>"] = "SPC",
-    -- ["<cr>"] = "RET",
-    -- ["<tab>"] = "TAB",
-  },
-  icons = {
-    breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
-    separator = "➜", -- symbol used between a key and it's label
-    group = "+", -- symbol prepended to a group
-  },
-  popup_mappings = {
-    scroll_down = "<c-d>", -- binding to scroll down inside the popup
-    scroll_up = "<c-u>", -- binding to scroll up inside the popup
-  },
-  window = {
-    border = "rounded", -- none, single, double, shadow
-    position = "bottom", -- bottom, top
-    margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-    padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-    winblend = 0,
-  },
   layout = {
-    height = { min = 4, max = 25 }, -- min and max height of the columns
-    width = { min = 20, max = 50 }, -- min and max width of the columns
-    spacing = 3, -- spacing between columns
-    align = "center", -- align columns left, center or right
+    align = "center",
   },
-  ignore_missing = true, -- enable this to hide mappings for which you didn't specify a label
-  hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
-  show_help = false, -- show help message on the command line when the popup is visible
-  -- triggers = "auto", -- automatically setup triggers
-  -- triggers = {"<leader>"} -- or specify a list manually
-  triggers_blacklist = {
-    -- list of mode / prefixes that should never be hooked by WhichKey
-    -- this is mostly relevant for key maps that start with a native binding
-    -- most people should not need to change this
-    i = { "j", "k" },
-    v = { "j", "k" },
-  },
-}--}}}
+  show_help = false,
+}
 
-local yank = function()--{{{
-  if vim.fn.mode() == 'n' then
-    vim.cmd('%y')
-  else
-    vim.cmd('normal! y')
-  end
-  vim.fn.system('bash ~/dotfiles/scripts.sh yank', vim.fn.getreg('0'))
-  vim.notify("copied to clipboard")
-end--}}}
-
-local opts = {--{{{
+local opts = {
   mode = "n", -- NORMAL mode
   prefix = "<leader>",
   buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
   silent = true, -- use `silent` when creating keymaps
   noremap = true, -- use `noremap` when creating keymaps
   nowait = true, -- use `nowait` when creating keymaps
-}--}}}
-local mappings = {--{{{
-  ["e"] = { "<cmd>NvimTreeToggle<cr>", "Explorer" },
-  ["w"] = { "<cmd>w<CR>", "Save" },
-  ["q"] = { "<cmd>q<CR>", "Quit" },
-  ["/"] = { '<cmd>lua require("Comment.api").toggle_current_linewise()<CR>', "Comment" },
-  ["R"] = { '<cmd>lua require("renamer").rename()<cr>', "Rename" },
-  ["z"] = { "<cmd>ZenMode<cr>", "Zen" },
-  ["y"] = { function() yank() end, "copy to clipboard" },
-  ["gy"] = "Link",
+}
 
-  b = {--{{{
-    name = "Buffer",
-    b = { "<cmd>lua require('telescope.builtin').buffers(require('telescope.themes').get_dropdown{previewer = false})<cr>", "Buffers" },
-    z = { "<cmd>NeoZoomToggle<CR>", "Zoom"},
-    [">"] = { "<cmd>BufferLineMoveNext<CR>", "Move right" },
-    ["<"] = { "<cmd>BufferLineMovePrev<CR>", "Move left" },
-  },--}}}
-
-  c = {--{{{
-  -- TODO:
-    name = "Compile",
-    C = { function() TOGGLE_FLOAT("./a.out") end, "Compile" },
-    c = { "Compile" },
-    r = { "Compile and run" },
-    m = { "Make" },
-  },--}}}
-
-  p = {--{{{
-    name = "Packer",
-    c = { "<cmd>PackerCompile<cr>", "Compile" },
-    i = { "<cmd>PackerInstall<cr>", "Install" },
-    s = { "<cmd>PackerSync<cr>", "Sync" },
-    S = { "<cmd>PackerStatus<cr>", "Status" },
-    u = { "<cmd>PackerUpdate<cr>", "Update" },
-  },--}}}
-
-  f = {--{{{
-    name = "Find",
-    B = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
-    c = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
-    C = { "<cmd>Telescope commands<cr>", "Commands" },
-    f = { "<cmd>Telescope find_files hidden=true<cr>", "Find files", },
-    F = { "<cmd>Telescope live_grep theme=ivy<cr>", "Find Text" },
-    s = { "<cmd>HopChar2<cr>", "Hop 2 characters" },
-    S = { "<cmd>HopWord<cr>", "Hop word" },
-    t = { "<cmd>TodoTelescope<cr>", "TODOs" },
-    h = { "<cmd>Telescope help_tags<cr>", "Help" },
-    i = { "<cmd>lua require('telescope').extensions.media_files.media_files()<cr>", "Media" },
-    l = { "<cmd>Telescope resume<cr>", "Last Search" },
-    M = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
-    r = { "<cmd>Telescope oldfiles<cr>", "Recent File" },
-    k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
-    p = { "<cmd>lua require('telescope').extensions.projects.projects()<cr>", "Projects" },
-  },--}}}
-
-  g = {--{{{
-    name = "Git",
-    g = { "<cmd>lua _LAZYGIT_TOGGLE()<CR>", "Lazygit" },
-    j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
-    k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
-    B = { "<cmd>GitBlameToggle<cr>", "Blame" },
-    p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
-    r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
-    R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
-    s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
-    u = {
-      "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
-      "Undo Stage Hunk",
-    },
-    o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
-    b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
-    c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
-    d = { "<cmd>Telescope git_status<cr>", "Diff: git status", },
-    D = {
-      "<cmd>Gitsigns diffthis HEAD<cr>",
-      "Diff",
-    },
-  },--}}}
-
-  l = {--{{{
-    name = "LSP",
-    a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
-    d = { "<cmd>TroubleToggle<cr>", "Diagnostics" },
-    w = {
-      "<cmd>Telescope lsp_workspace_diagnostics<cr>",
-      "Workspace Diagnostics",
-    },
-    f = { "<cmd>lua vim.lsp.buf.formatting()<cr>", "Format" },
-    F = { "<cmd>LspToggleAutoFormat<cr>", "Toggle Autoformat" },
-    i = { "<cmd>LspInfo<cr>", "Info" },
-    I = { "<cmd>LspInstallInfo<cr>", "Installer Info" },
-    j = {
-      "<cmd>lua vim.diagnostic.goto_next({buffer=0})<CR>",
-      "Next Diagnostic",
-    },
-    k = {
-      "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>",
-      "Prev Diagnostic",
-    },
-    h = { "<cmd>Lspsaga hover_doc<cr>", "hover doc" },
-    p = { "<cmd>Lspsaga preview_definition<cr>", "preview definition" },
-    l = { "<cmd>lua vim.lsp.codelens.run()<cr>", "CodeLens Action" },
-    o = { "<cmd>TagbarToggle<cr>", "Outline(tagbar)" },
-    O = { "<cmd>SymbolsOutline<cr>", "Outline(SymbolsOutline)" },
-    q = { "<cmd>lua vim.lsp.diagnostic.set_loclist()<cr>", "Quickfix" },
-    r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
-    R = { "<cmd>TroubleToggle lsp_references<cr>", "References" },
-    s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
-    S = {
-      "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
-      "Workspace Symbols",
-    },
-    ['.'] = { function() vim.diagnostic.open_float() end, "Diagnostic under cursor" },
-  },--}}}
-
-  S = {--{{{
-    name = "SnipRun",
-    c = { "<cmd>SnipClose<cr>", "Close" },
-    f = { "<cmd>%SnipRun<cr>", "Run File" },
-    i = { "<cmd>SnipInfo<cr>", "Info" },
-    m = { "<cmd>SnipReplMemoryClean<cr>", "Mem Clean" },
-    r = { "<cmd>SnipReset<cr>", "Reset" },
-    t = { "<cmd>SnipRunToggle<cr>", "Toggle" },
-    x = { "<cmd>SnipTerminate<cr>", "Terminate" },
-  },--}}}
-
-  t = {--{{{
-    name = "Terminal",
-    ["1"] = { ":1ToggleTerm<cr>", "1" },
-    ["2"] = { ":2ToggleTerm<cr>", "2" },
-    ["3"] = { ":3ToggleTerm<cr>", "3" },
-    ["t"] = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
-    ["h"] = { function() TOGGLE_FLOAT('htop') end, "Htop" },
-    ["u"] = { function() TOGGLE_FLOAT('ncdu') end, "NCDU" },
-    ["p"] = { function() TOGGLE_FLOAT('python3') end, "Python" },
-    ["-"] = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
-    ["\\"] = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
-  },--}}}
-
-  T = {--{{{
-    name = "Treesitter",
-    h = { "<cmd>TSHighlightCapturesUnderCursor<cr>", "Highlight" },
-    p = { "<cmd>TSPlaygroundToggle<cr>", "Playground" },
-  },--}}}
-}--}}}
-
-local vopts = {--{{{
-  mode = "v", -- VISUAL mode
-  prefix = "<leader>",
-  buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-  silent = true, -- use `silent` when creating keymaps
-  noremap = true, -- use `noremap` when creating keymaps
-  nowait = true, -- use `nowait` when creating keymaps
-}--}}}
-local vmappings = {--{{{
-  ["/"] = { '<ESC><CMD>lua require("Comment.api").toggle_linewise_op(vim.fn.visualmode())<CR>', "Comment" },
-  ["s"] = { "<esc><cmd>'<,'>SnipRun<cr>", "Run range" },
-  ["y"] = { function() yank() end, "copy to clipboard" },
-}--}}}
-
-local mopts = {--{{{
-  mode = "n", -- NORMAL mode
-  prefix = "m",
-  buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-  silent = true, -- use `silent` when creating keymaps
-  noremap = true, -- use `noremap` when creating keymaps
-  nowait = true, -- use `nowait` when creating keymaps
-}--}}}
-local mmappings = {--{{{
-  a = { "<cmd>BookmarkAnnotate<cr>", "Annotate" },
-  c = { "<cmd>BookmarkClear<cr>", "Clear" },
-  m = { "<cmd>BookmarkToggle<cr>", "Toggle" },
-  h = { '<cmd>lua require("harpoon.mark").add_file()<cr>', "Harpoon" },
-  n = { "<cmd>BookmarkNext<cr>", "Next" },
-  p = { "<cmd>BookmarkPrev<cr>", "Prev" },
-  s = {
-    "<cmd>lua require('telescope').extensions.vim_bookmarks.all({ hide_filename=false, prompt_title=\"bookmarks\", shorten_path=false })<cr>",
-    "Show",
-  },
-  x = { "<cmd>BookmarkClearAll<cr>", "Clear All" },
-  u = { '<cmd>lua require("harpoon.ui").toggle_quick_menu()<cr>', "Harpoon UI" },
-}--}}}
+local mappings = {
+  c = { name = "Compile" }, -- TODO:
+  b = { name = "Buffer" },
+  p = { name = "Packer" },
+  f = { name = "Find" },
+  g = { name = "Git" },
+  l = { name = "LSP" },
+  s = { name = "SnipRun" },
+  t = { name = "Terminal" },
+}
 
 which_key.register(mappings, opts)
-which_key.register(vmappings, vopts)
-which_key.register(mmappings, mopts)
--- vim/.config/nvim/lua/mapping.lua
+++ vim/.config/nvim/lua/mapping.lua
@@ -1,10 +1,25 @@
-local opts = { noremap = true, silent = true }
-local keymap = vim.keymap.set
+-- TODO: smart split plugin
+-- FIX: telescope config is not properly loaded: <C-p>
 
---Remap space as leader key
-keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+local function map(mode, lhs, rhs, desc)
+  vim.keymap.set(mode, lhs, rhs, {
+    silent = true,
+    desc = desc,
+  })
+end
+
+local function yank()
+  if vim.fn.mode() == 'n' then
+    vim.cmd('%y')
+  else
+    vim.cmd('normal! y')
+  end
+  vim.fn.system('bash ~/dotfiles/scripts.sh yank', vim.fn.getreg('0'))
+  vim.notify("copied to clipboard")
+end
 
 -- Modes
 --   "n": normal_mode
@@ -15,53 +30,156 @@ vim.g.maplocalleader = " "
 --   "c": command_mode
 --   "!": insert_command_mode
 
--- Normal --
-keymap("n", "<C-j>", "4jzz", opts)
-keymap("n", "<C-k>", "4kzz", opts)
-keymap("v", "<C-j>", "4jzz", opts)
-keymap("v", "<C-k>", "4kzz", opts)
-keymap("n", "H", "<cmd>BufferLineCyclePrev<CR>", opts)
-keymap("n", "L", "<cmd>BufferLineCycleNext<CR>", opts)
-keymap("n", "]q", "<cmd>cnext<CR>", opts)
-keymap("n", "[q", "<cmd>cprev<CR>", opts)
+-- <leader>: normal mode{{{
+map("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", "Explorer")
+map("n", "<leader>w", "<cmd>w<CR>", "Save")
+map("n", "<leader>q", "<cmd>q<CR>", "Quit")
+map("n", "<leader>/", '<cmd>lua require("Comment.api").toggle_current_linewise()<CR>', "Comment")
+map("n", "<leader>z", "<cmd>ZenMode<cr>", "Zen mode")
+map("n", "<leader>y", function() yank() end, "copy to clipboard")
+--}}}
 
-keymap("!", "<C-a>", "<Home>", opts)
-keymap("!", "<C-e>", "<End>", opts)
+-- <leader>b: buffer{{{
+map("n", "<leader>bb", "<cmd>lua require('telescope.builtin').buffers(require('telescope.themes').get_dropdown{previewer = false})<cr>", "Buffers")
+map("n", "<leader>b>", "<cmd>BufferLineMoveNext<CR>", "Move right")
+map("n", "<leader>b<", "<cmd>BufferLineMovePrev<CR>", "Move left")
+--}}}
 
--- Resize with arrows
--- TODO: smart split plugin
-keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+-- <leader>p: packer{{{
+map("n", "<leader>pc", "<cmd>PackerCompile<cr>", "Compile")
+map("n", "<leader>pi", "<cmd>PackerInstall<cr>", "Install")
+map("n", "<leader>ps", "<cmd>PackerSync<cr>", "Sync")
+map("n", "<leader>pS", "<cmd>PackerStatus<cr>", "Status")
+map("n", "<leader>pu", "<cmd>PackerUpdate<cr>", "Update")
+--}}}
 
--- Insert --
-keymap("i", "kj", "<ESC>", opts)
+-- <leader>f: find{{{
+map("n", "<leader>fB", "<cmd>Telescope git_branches<cr>", "Checkout branch")
+map("n", "<leader>fc", "<cmd>Telescope colorscheme<cr>", "Colorscheme")
+map("n", "<leader>fC", "<cmd>Telescope commands<cr>", "Commands")
+map("n", "<leader>ff", "<cmd>Telescope find_files hidden=true<cr>", "Find files")
+map("n", "<leader>fF", "<cmd>Telescope live_grep theme=ivy<cr>", "Find Text")
+map("n", "<leader>fs", "<cmd>HopChar2<cr>", "Hop 2 characters")
+map("n", "<leader>fS", "<cmd>HopWord<cr>", "Hop word")
+map("n", "<leader>ft", "<cmd>TodoTelescope<cr>", "TODOs")
+map("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", "Help")
+map("n", "<leader>fl", "<cmd>Telescope resume<cr>", "Last Search")
+map("n", "<leader>fM", "<cmd>Telescope man_pages<cr>", "Man Pages")
+map("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", "Recent File")
+map("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", "Keymaps")
+map("n", "<leader>fp", "<cmd>lua require('telescope').extensions.projects.projects()<cr>", "Projects")
+--}}}
 
--- Visual --
+-- <leader>g: git{{{
+map("n", "<leader>gj", "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk")
+map("n", "<leader>gk", "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk")
+map("n", "<leader>gB", "<cmd>GitBlameToggle<cr>", "Blame")
+map("n", "<leader>gp", "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk")
+map("n", "<leader>gr", "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk")
+map("n", "<leader>gR", "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer")
+map("n", "<leader>gs", "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk")
+map("n", "<leader>gu", "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>", "Undo Stage Hunk")
+map("n", "<leader>go", "<cmd>Telescope git_status<cr>", "git status")
+map("n", "<leader>gb", "<cmd>Telescope git_branches<cr>", "Checkout branch")
+map("n", "<leader>gc", "<cmd>Telescope git_commits<cr>", "Checkout commit")
+map("n", "<leader>gd", "<cmd>Gitsigns diffthis HEAD<cr>", "Diff")
+--}}}
+
+-- <leader>l: lsp{{{
+map("n", "K", "<cmd>Lspsaga hover_doc<cr>","hover document")
+map("i", "<C-k>", vim.lsp.buf.signature_help, "signature_help")
+map("n", "go", vim.diagnostic.open_float, "Hover diagnostics")
+map("n", "gd", vim.lsp.buf.definition, "go to definition")
+map("n", "gD", vim.lsp.buf.declaration, "go to declaration")
+map("n", "gr", vim.lsp.buf.references, "go to references")
+map("n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action")
+map("n", "<leader>ld", "<cmd>TroubleToggle<cr>", "Diagnostics")
+map("n", "<leader>lw", "<cmd>Telescope lsp_workspace_diagnostics<cr>", "Workspace Diagnostics")
+map("n", "<leader>lf", vim.lsp.buf.formatting, "Format")
+map("n", "<leader>lF", "<cmd>LspToggleAutoFormat<cr>", "Toggle Autoformat")
+map("n", "<leader>li", "<cmd>LspInfo<cr>", "Info")
+map("n", "<leader>lI", "<cmd>LspInstallInfo<cr>", "Installer Info")
+map("n", "<leader>lj", "<cmd>lua vim.diagnostic.goto_next({buffer=0})<CR>")
+map("n", "<leader>lk", "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<CR>")
+map("n", "<leader>lp", "<cmd>Lspsaga preview_definition<cr>", "preview definition")
+map("n", "<leader>ll", "<cmd>lua vim.lsp.codelens.run()<cr>", "CodeLens Action")
+map("n", "<leader>lo", "<cmd>TagbarToggle<cr>", "Outline(tagbar)")
+map("n", "<leader>lO", "<cmd>SymbolsOutline<cr>", "Outline(SymbolsOutline)")
+map("n", "<leader>lq", "<cmd>lua vim.lsp.diagnostic.set_loclist()<cr>", "Quickfix")
+map("n", "<leader>lr", '<cmd>Lspsaga rename<cr>', "Rename")
+map("n", "<leader>lR", "<cmd>TroubleToggle lsp_references<cr>", "References")
+map("n", "<leader>ls", "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols")
+map("n", "<leader>lS", "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", "Workspace Symbols")
+--}}}
+
+-- <leader>s: sniprun{{{
+map("n", "<leader>sc", "<cmd>SnipClose<cr>", "Close")
+map("n", "<leader>sf", "<cmd>%SnipRun<cr>", "Run File")
+map("n", "<leader>si", "<cmd>SnipInfo<cr>", "Info")
+map("n", "<leader>sm", "<cmd>SnipReplMemoryClean<cr>", "Mem Clean")
+map("n", "<leader>sr", "<cmd>SnipReset<cr>", "Reset")
+map("n", "<leader>st", "<cmd>SnipRunToggle<cr>", "Toggle")
+map("n", "<leader>sx", "<cmd>SnipTerminate<cr>", "Terminate")
+--}}}
+
+-- <leader>t: terminal{{{
+map("n", "<leader>t1", ":1ToggleTerm<cr>", "1")
+map("n", "<leader>t2", ":2ToggleTerm<cr>", "2")
+map("n", "<leader>t3", ":3ToggleTerm<cr>", "3")
+map("n", "<leader>tt", "<cmd>ToggleTerm direction=float<cr>", "Float")
+map("n", "<leader>th", function() TOGGLE_FLOAT('htop') end, "Htop")
+map("n", "<leader>tu", function() TOGGLE_FLOAT('ncdu') end, "NCDU")
+map("n", "<leader>tp", function() TOGGLE_FLOAT('python3') end, "Python")
+map("n", "<leader>t-", "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal")
+map("n", [[<leader>t\]], "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical")
+--}}}
+
+-- <leader>: visual mode{{{
+map("v", "<leader>/", '<ESC><CMD>lua require("Comment.api").toggle_linewise_op(vim.fn.visualmode())<CR>', "Comment")
+map("v", "<leader>s", "<esc><cmd>'<,'>SnipRun<cr>", "Run range")
+map("v", "<leader>y", function() yank() end, "copy to clipboard")
+--}}}
+
+-- m: mark mappings{{{
+map("n", "ma", "<cmd>BookmarkAnnotate<cr>", "Annotate")
+map("n", "mc", "<cmd>BookmarkClear<cr>", "Clear")
+map("n", "mm", "<cmd>BookmarkToggle<cr>", "Toggle")
+map("n", "mn", "<cmd>BookmarkNext<cr>", "Next")
+map("n", "mp", "<cmd>BookmarkPrev<cr>", "Prev")
+map("n", "ms", "<cmd>lua require('telescope').extensions.vim_bookmarks.all({ hide_filename=false, prompt_title=\"bookmarks\", shorten_path=false })<cr>", "Show")
+map("n", "mx", "<cmd>BookmarkClearAll<cr>", "Clear All")
+--}}}
+
+-- others{{{
+map("i", "kj", "<ESC>")
+
+-- quickfix
+map("n", "]q", "<cmd>cnext<CR>", "quickfix next")
+map("n", "[q", "<cmd>cprev<CR>", "quickfix prev")
 
 -- Move text up and down
-keymap("v", "<S-h>", ":m '<-2<CR>gv=gv", opts)
-keymap("v", "<S-l>", ":m '>+1<CR>gv=gv", opts)
--- keymap("v", "p", '"_dP', opts)
+map("v", "<S-h>", ":m '<-2<CR>gv=gv")
+map("v", "<S-l>", ":m '>+1<CR>gv=gv")
 
--- Terminal --
-keymap('t', '<esc>', [[<C-\><C-n>]], opts)
-keymap('t', 'kj', [[<C-\><C-n>]], opts)
+-- terminal
+map('t', '<esc>', [[<C-\><C-n>]])
+map('t', 'kj', [[<C-\><C-n>]])
 
--- Custom
-keymap("n", "Q", "<cmd>Bdelete<CR>", opts)
-keymap("n", "<F7>", "<cmd>TSHighlightCapturesUnderCursor<cr>", opts)
-keymap(
-  "n",
-  "<C-p>",
-  "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>",
-  opts
-)
-keymap("n", "<C-t>", "<cmd>lua vim.lsp.buf.document_symbol()<cr>", opts)
-keymap("n", "gx", [[:silent execute '!$BROWSER ' . shellescape(expand('<cfile>'), 1)<CR>]], opts)
+-- telescope
+map("n", "<C-p>", "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>")
 
--- Seamless navigation between vim and tmux
+-- bufferline
+map("n", "H", "<cmd>BufferLineCyclePrev<CR>")
+map("n", "L", "<cmd>BufferLineCycleNext<CR>")
+map("n", "Q", "<cmd>Bdelete<CR>")
+
+-- navigation
+map("n", "<C-j>", "4jzz")
+map("n", "<C-k>", "4kzz")
+map("v", "<C-j>", "4jzz")
+map("v", "<C-k>", "4kzz")
+map("!", "<C-a>", "<Home>")
+map("!", "<C-e>", "<End>")
 vim.cmd [[
   function! Navigation_vim_tmux(vim_dir)
     let pre_winnr=winnr()
@@ -76,3 +194,4 @@ vim.cmd [[
   nnoremap <silent><M-k> :call Navigation_vim_tmux("k")<CR>
   nnoremap <silent><M-l> :call Navigation_vim_tmux("l")<CR>
 ]]
+--}}}
