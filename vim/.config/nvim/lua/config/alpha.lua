82e54994a93c8c7a10cf6d1d769ecf92b2c4550e
williamhsieh <wh31110@gmail.com>
2022-05-26 14:05:31 +0800

    [FEAT] dynamic padding

--- vim/.config/nvim/lua/config/alpha.lua
+++ vim/.config/nvim/lua/config/alpha.lua
@@ -1,23 +1,6 @@
 local alpha = require "alpha"
 local icons = require "icons"
-
-local function footer()
-  local plugins_cnt = #vim.fn.globpath(vim.fn.stdpath "data" .. "/site/pack/packer/start", "*", 0, 1)
-  return plugins_cnt .. " plugins loaded"
-end
-
 local dashboard = require "alpha.themes.dashboard"
-
--- https://manytools.org/hacker-tools/ascii-banner/
-dashboard.section.header.val = {
-  "", "", "", "", "", "",
-  "███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗",
-  "████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║",
-  "██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║",
-  "██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║",
-  "██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║",
-  "╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝",
-}
 
 dashboard.section.buttons.val = {
   dashboard.button("i", icons.ui.NewFile .. " New file", ":ene <BAR> startinsert <CR>"),
@@ -29,11 +12,38 @@ dashboard.section.buttons.val = {
   dashboard.button("q", icons.diagnostics.Error .. " Quit", ":qa<CR>"),
 }
 
-dashboard.section.footer.val = footer()
-dashboard.section.footer.opts.hl = "Type"
+dashboard.section.header.val = function()
+  -- https://manytools.org/hacker-tools/ascii-banner/
+  local banner = {
+    "███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗",
+    "████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║",
+    "██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║",
+    "██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║",
+    "██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║",
+    "╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝",
+  }
+
+  local height = vim.fn.winheight(0)
+  local content = #banner + 2 * #dashboard.section.buttons.val + 5
+  local cnt = vim.fn.max{1, vim.fn.floor((height - content) * 0.4)}
+
+  local res = {}
+  for _ = 1, cnt do
+    table.insert(res, "")
+  end
+  for _, v in pairs(banner) do
+    table.insert(res, v)
+  end
+  return res
+end
+
+dashboard.section.footer.val = function()
+  local cnt = #vim.fn.globpath(vim.fn.stdpath "data" .. "/site/pack/packer/start", "*", 0, 1)
+  return cnt .. " plugins loaded"
+end
+
 dashboard.section.header.opts.hl = "Include"
+dashboard.section.footer.opts.hl = "Type"
 dashboard.section.buttons.opts.hl = "Keyword"
 
-dashboard.opts.opts.noautocmd = true
--- vim.cmd([[autocmd User AlphaReady echo 'ready']])
 alpha.setup(dashboard.opts)
