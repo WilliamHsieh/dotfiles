aaa6ebc421657e1d59d8ce643cafaf4281f26f6f
williamhsieh <wh31110@gmail.com>
2022-08-01 13:34:01 +0800

    [MISC] move into core directory

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -5,16 +5,27 @@ else
   impatient.enable_profile()
 end
 
--- TODO: try out lazygit
--- TODO: simplified chinese is not properly shown
--- TODO: C-a and C-e in command line mode feels wierd
--- TODO: sessionloadpost open nvimtree: https://github.com/Shatur/neovim-session-manager
--- TODO: auto cd into current file (issue: tmuxconfig will not show branch on lualine properly)
+require "core.setting"
+require "core.plugins"
+require "core.autocmd"
+require "core.mapping"
+
+-- FIX: speed up the startuptime!!
+-- FIX: disable treesitter, ftplugin when file is too large
 -- FIX: typing tab in insert mode is jumping everywhere
+-- FIX: "C-a" and C-e in command line mode feels wierd
+-- FIX: bufferline is showing strange buffer when entering netrw
+-- FIX: make lualine x optional when there's no more space to display
+-- FIX: default behavior for tab (happens when actually trying to type a tab)
+-- TODO: smart split plugin
+-- TODO: make <S-k> show vim document in lua/.vimrc file
+-- TODO: simplified chinese is not properly shown
+-- TODO: auto cd into current file (issue: tmuxconfig will not show branch on lualine properly)
+-- TODO: how did makefile keep track of file update since last compilation?
 -- NOTE: https://github.com/AstroNvim/AstroNvim
 -- NOTE: https://github.com/nvim-lua/kickstart.nvim
-
-require "setting"
-require "plugins"
-require "autocmd"
-require "mapping"
+-- TODO: execute previous command in termexec
+-- TODO: feline
+-- TODO: disable highlight under cursor in toggleterm filetype
+-- NOTE: try barbar.nvim
+-- NOTE: ui selection: https://github.com/stevearc/dressing.nvim
