79a9ba400cba7d4c6162e77a38eca74f0be50003
williamhsieh <wh31110@gmail.com>
2022-08-14 15:26:36 +0800

    [FEAT] get hex color from highlight groups

--- vim/.config/nvim/lua/utils.lua
+++ vim/.config/nvim/lua/utils.lua
@@ -6,4 +6,13 @@ function utils.insert_table(ts)
   end
 end
 
+function utils.get_hl(group)
+  local hl = vim.api.nvim_get_hl_by_name(group, true)
+  local function to_hex(color)
+    return color and string.format("#%06x", color) or ''
+  end
+  return { fg = to_hex(hl.foreground), bg = to_hex(hl.background) }
+end
+
+
 return utils
