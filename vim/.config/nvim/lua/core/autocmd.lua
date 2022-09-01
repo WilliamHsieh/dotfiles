30a506a6b02c45d55ab6832296167cb3a478683d
williamhsieh <wh31110@gmail.com>
2022-09-01 13:30:10 +0800

    [FIX] adding lspsaga filetype

--- vim/.config/nvim/lua/core/autocmd.lua
+++ vim/.config/nvim/lua/core/autocmd.lua
@@ -19,7 +19,7 @@ autocmd BufRead * autocmd FileType <buffer> ++once
 
   augroup _general_settings
     autocmd!
-    autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR> 
+    autocmd FileType qf,help,man,lspinfo,LspsagaHover nnoremap <silent> <buffer> q :close<CR> 
     autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200}) 
     autocmd BufWinEnter * :set formatoptions-=cro
     autocmd FileType qf set nobuflisted
