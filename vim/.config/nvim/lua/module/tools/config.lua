fc9db49cf18bfc77f1f643dcbb603ed4d9e7ddcc
WilliamHsieh <wh31110@gmail.com>
2022-11-05 14:31:00 +0800

    feat(neogit): update config

--- vim/.config/nvim/lua/module/tools/config.lua
+++ vim/.config/nvim/lua/module/tools/config.lua
@@ -124,6 +124,8 @@ end
 
 function config.neogit()
   require("neogit").setup {
+    disable_commit_confirmation = true,
+    disable_insert_on_commit = false,
     integrations = {
       diffview = true
     },
