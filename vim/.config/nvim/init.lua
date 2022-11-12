e86dd703dfa49c28c571ef9253a2e4ee5fbd54db
williamhsieh <wh31110@gmail.com>
2022-11-12 14:50:22 +0800

    chore: remove install prompt

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -9,10 +9,8 @@ require "core.mapping"
 
 local packer_path = vim.fn.stdpath "data" .. "/site/pack/packer/opt/packer.nvim"
 if vim.fn.empty(vim.fn.glob(packer_path)) > 0 then
-  vim.notify("Installing plugins...")
   vim.fn.system { "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", packer_path }
-
-  vim.cmd("packadd packer.nvim")
+  vim.cmd.packadd("packer.nvim")
   require("core.plugins")
   require("packer").sync()
 
