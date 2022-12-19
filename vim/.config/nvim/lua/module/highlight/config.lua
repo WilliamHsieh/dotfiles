f7129dbe714bf596139b374edaed166d67e74ac3
williamhsieh <wh31110@gmail.com>
2022-12-19 15:08:32 +0800

    fix(ts): disable ts highlight for latex and large files

--- vim/.config/nvim/lua/module/highlight/config.lua
+++ vim/.config/nvim/lua/module/highlight/config.lua
@@ -7,6 +7,13 @@ function config.treesitter()
     ignore_install = { "" },
     highlight = {
       enable = true,
+      disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if lang == "latex" or ok and stats and stats.size > max_filesize then
+          return true
+        end
+      end,
       additional_vim_regex_highlighting = false,
     },
     autopairs = {
