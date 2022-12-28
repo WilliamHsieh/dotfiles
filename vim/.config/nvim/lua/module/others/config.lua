c7c0110e7a8b91fb0433860c5a248ec08e570b2a
williamhsieh <wh31110@gmail.com>
2022-12-28 20:00:00 +0800

    feat(vimtex): toc format

--- vim/.config/nvim/lua/module/others/config.lua
+++ vim/.config/nvim/lua/module/others/config.lua
@@ -18,6 +18,14 @@ function config.vimtex()
   vim.g.vimtex_view_skim_sync = 1
   vim.g.vimtex_view_skim_activate = 1
   vim.g.vimtex_quickfix_open_on_warning = 0
+  vim.g.vimtex_toc_config = {
+    indent_levels = 1,
+    show_help = 0,
+    layers = {
+      'content',
+    }
+  }
+  -- nvim --headless -c "VimtexInverseSearch %line '%file'"
 end
 
 return config
