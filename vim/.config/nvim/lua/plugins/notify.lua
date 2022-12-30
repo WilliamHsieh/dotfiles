c0e4b411cc5644a36c07e8500dbd1f478b117e01
williamhsieh <wh31110@gmail.com>
2022-12-30 22:49:48 +0800

    perf: lazy load notify

--- vim/.config/nvim/lua/plugins/notify.lua
+++ vim/.config/nvim/lua/plugins/notify.lua
@@ -1,26 +1,29 @@
 local M = {
   "rcarriga/nvim-notify",
-  event = "VeryLazy",
 }
 
-function M.config()
-  local icons = require "core.icons"
-  vim.notify = require("notify")
+function M.init()
+  vim.notify = function(...)
+    local icons = require "core.icons"
+    vim.notify = require("notify")
 
-  vim.notify.setup {
-    stages = "fade_in_slide_out",
-    minimum_width = 10,
-    icons = {
-      ERROR = icons.diagnostics.Error,
-      WARN = icons.diagnostics.Warning,
-      INFO = icons.diagnostics.Information,
-      DEBUG = icons.ui.Bug,
-      TRACE = icons.ui.Pencil,
-    },
-    on_open = function (win)
-      vim.api.nvim_win_set_config(win, { focusable = false })
-    end,
-  }
+    vim.notify.setup {
+      stages = "fade_in_slide_out",
+      minimum_width = 10,
+      icons = {
+        ERROR = icons.diagnostics.Error,
+        WARN = icons.diagnostics.Warning,
+        INFO = icons.diagnostics.Information,
+        DEBUG = icons.ui.Bug,
+        TRACE = icons.ui.Pencil,
+      },
+      on_open = function (win)
+        vim.api.nvim_win_set_config(win, { focusable = false })
+      end,
+    }
+
+    vim.notify(...)
+  end
 end
 
 return M
