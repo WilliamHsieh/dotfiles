bf6ea8b8cb4d241f8dd3ceb61c8aef199dc0b804
William Hsieh <wh31110@gmail.com>
2023-01-03 00:43:56 +0800

    fix(startuptime): measure ten times average

--- vim/.config/nvim/lua/plugins/init.lua
+++ vim/.config/nvim/lua/plugins/init.lua
@@ -159,7 +159,10 @@ return {
 
   {
     "dstein64/vim-startuptime",
-    cmd = "StartupTime"
+    cmd = "StartupTime",
+    config = function()
+      vim.g.startuptime_tries = 10
+    end
   },
 
   {
