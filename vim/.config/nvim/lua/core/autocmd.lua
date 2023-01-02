c0ee614cb3581adeb1ad2170d213dcd3b5061d75
William Hsieh <wh31110@gmail.com>
2023-01-03 01:12:39 +0800

    feat(autocmd): automatically goto previous position

--- vim/.config/nvim/lua/core/autocmd.lua
+++ vim/.config/nvim/lua/core/autocmd.lua
@@ -27,6 +27,16 @@ autocmd("VimResized", {
   group = "config_group",
   command = "tabdo wincmd ="
 })
+autocmd("BufReadPost", {
+  desc = "goto previous position",
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
 
 autocmd("User", {
   pattern = "AlphaReady",
