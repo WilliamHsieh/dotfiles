be0ced8e7614e4ea2184dfee3e09babf411837f5
williamhsieh <wh31110@gmail.com>
2023-01-14 13:33:17 +0800

    fix(vimtex): better visual mode jk

--- vim/.config/nvim/after/ftplugin/tex.lua
+++ vim/.config/nvim/after/ftplugin/tex.lua
@@ -5,8 +5,8 @@ vim.opt_local.conceallevel = 1
 vim.g.tex_conceal = 'abdmg'
 
 -- mappings
-vim.keymap.set("n", "j", "gj")
-vim.keymap.set("n", "k", "gk")
+vim.keymap.set({ "n", "v" }, "j", "gj")
+vim.keymap.set({ "n", "v" }, "k", "gk")
 
 local ok, wk = pcall(require, "which-key")
 if ok then
