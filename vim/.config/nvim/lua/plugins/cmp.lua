2d844b864bba0e6bf697248e33b83671a908e47e
williamhsieh <wh31110@gmail.com>
2023-01-25 15:05:38 +0800

    fix(cmp): only jump on <tab> when cursor is inside snippet

--- vim/.config/nvim/lua/plugins/cmp.lua
+++ vim/.config/nvim/lua/plugins/cmp.lua
@@ -44,7 +44,7 @@ function M.config()
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
+      elseif luasnip.expand_or_locally_jumpable() then
         luasnip.expand_or_jump()
       elseif has_words_before() then
         cmp.complete()
