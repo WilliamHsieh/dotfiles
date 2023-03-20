356543fb5a5219f51e9c3771b2b04f50bac30afb
William Hsieh <wh31110@gmail.com>
2023-03-20 16:00:04 +0800

    feat(nix): git config, delta, bat

--- home.nix
+++ home.nix
@@ -4,22 +4,71 @@
     homeDirectory = "/home/william";
     stateVersion = "22.11";
     packages = with pkgs; [
+      # essential
       git
-      exa
-      trash-cli
 
+      # editor
       neovim
       ripgrep
       fd
       unzip
 
+      # shell
       zsh
       starship
+      exa
+      trash-cli
 
+      # terminal multiplexer
       tmux
       fzf
+
+      # common tools
+      direnv
+      jq
+      bat
+      delta
+      comma
+      htop
     ];
   };
 
   programs.home-manager.enable = true;
+
+  programs.direnv.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName = "William Hsieh";
+    userEmail = "wh31110@gmail.com";
+    delta = {
+      enable = true;
+      options = {
+        true-color = "always";
+        syntax-theme = "base16-256";
+      };
+    };
+    ignores = [
+      "*.swp"
+      ".DS_Store"
+    ];
+    extraConfig = {
+      merge = {
+        tool = "vimdiff";
+        conflictstyle = "diff3";
+      };
+      pull = {
+        rebase=true;
+      };
+      mergetool.prompt = "false";
+    };
+  };
+
+  programs.bat = {
+    enable = true;
+    config = {
+      # TODO: https://github.com/catppuccin/bat
+      theme = "base16-256";
+    };
+  };
 }
