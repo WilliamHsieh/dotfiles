4916295b2e6b65cd44f4df1a5526657e99b520a4
William Hsieh <wh31110@gmail.com>
2023-03-20 14:30:37 +0800

    chore: update plugins
    - https://github.com/folke/which-key.nvim/pull/363

--- config/.config/nvim/lua/plugins/whichkey.lua
+++ config/.config/nvim/lua/plugins/whichkey.lua
@@ -1,6 +1,6 @@
 local M = {
-  "williamhsieh/which-key.nvim",
-  keys = { "<leader>", '"', "'", "g", "m", "z" },
+  "folke/which-key.nvim",
+  event = "VeryLazy",
 }
 
 function M.config()
@@ -10,15 +10,6 @@ function M.config()
     plugins = {
       spelling = {
         enabled = true,
-      },
-      presets = {
-        operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
-        motions = false, -- adds help for motions
-        text_objects = false, -- help for text objects triggered after entering an operator
-        windows = true, -- default bindings on <c-w>
-        nav = true, -- misc bindings to work with windows
-        z = true, -- bindings for folds, spelling and others prefixed with z
-        g = true, -- bindings for prefixed with g
       },
     },
     layout = {
