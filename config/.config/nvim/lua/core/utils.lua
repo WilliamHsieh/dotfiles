7cd04e0ecc11b384a5a085d53ee8ecc73df2ab4e
William Hsieh <wh31110@gmail.com>
2023-11-08 11:50:56 +0800

    fix(hl): replace the deprecated api

--- config/.config/nvim/lua/core/utils.lua
+++ config/.config/nvim/lua/core/utils.lua
@@ -2,11 +2,11 @@ local utils = {}
 
 -- show all highlight groups by sourcing $VIMRUNTIME/syntax/hitest.vim
 function utils.get_hl(group)
-  local hl = vim.api.nvim_get_hl_by_name(group, true)
+  local hl = vim.api.nvim_get_hl(0, { name = group, link = true })
   local function to_hex(color)
     return color and string.format("#%06x", color) or ''
   end
-  return { fg = to_hex(hl.foreground), bg = to_hex(hl.background) }
+  return { fg = to_hex(hl.fg), bg = to_hex(hl.bg) }
 end
 
 function utils.get_tmux_option(opt)
