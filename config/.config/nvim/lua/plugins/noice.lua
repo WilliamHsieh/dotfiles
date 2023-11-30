8c9e77ccf7db1d9c5edb53dc6b6b1351f4c4d917
William Hsieh <wh31110@gmail.com>
2023-11-30 14:12:57 +0800

    feat(noice): move undo message to mini view && lazy loading

--- config/.config/nvim/lua/plugins/noice.lua
+++ config/.config/nvim/lua/plugins/noice.lua
@@ -1,70 +1,65 @@
 local M = {
   "folke/noice.nvim",
-  priority = 100,
+  event = "VeryLazy",
   dependencies = {
     "MunifTanjim/nui.nvim",
     "rcarriga/nvim-notify",
   },
 }
 
-function M.config()
-  require("noice").setup {
-    messages = {
-      view_search = false,
-      view_error = "mini",
-      view_warn = "mini",
+M.opts = {
+  messages = {
+    view_search = false,
+    view_error = "mini",
+    view_warn = "mini",
+  },
+  commands = {
+    history = {
+      view = "popup",
     },
-    commands = {
-      history = {
-        view = "popup",
-      },
+  },
+  lsp = {
+    hover = {
+      enabled = false,
     },
-    lsp = {
-      hover = {
-        enabled = false,
-      },
-      signature = {
-        enabled = false,
-      },
-      override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
-        ["cmp.entry.get_documentation"] = true,
-      },
+    signature = {
+      enabled = false,
     },
-    presets = {
-      command_palette = true, -- position the cmdline and popupmenu together
-      long_message_to_split = true, -- long messages will be sent to a split
-      inc_rename = true, -- enables an input dialog for inc-rename.nvim
-      lsp_doc_border = true, -- add a border to hover docs and signature help
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true,
     },
-    routes = {
-      {
-        -- don't show written messages
-        filter = {
-          event = "msg_show",
-          kind = "",
-          find = "written",
+  },
+  presets = {
+    command_palette = true,       -- position the cmdline and popupmenu together
+    long_message_to_split = true, -- long messages will be sent to a split
+    inc_rename = true,            -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = true,        -- add a border to hover docs and signature help
+  },
+  routes = {
+    {
+      filter = {
+        event = "msg_show",
+        kind = "",
+        any = {
+          { find = "%d+L, %d+B written" },
         },
-        opts = { skip = true },
       },
-      {
-        -- don't show message on change `u` or `<C-R>`
-        filter = {
-          event = "msg_show",
-          find = "; %a* #%d",
-        },
-        opts = { skip = true },
-      },
-      {
-        filter = {
-          event = "msg_show",
-          find = "Already at %a* change",
-        },
-        opts = { skip = true },
-      },
+      opts = { skip = true },
     },
-  }
-end
+    {
+      filter = {
+        event = "msg_show",
+        any = {
+          { find = "; after #%d+" },
+          { find = "; before #%d+" },
+          { find = "Already at %a* change" },
+        },
+      },
+      view = "mini",
+    },
+  },
+}
 
 return M
