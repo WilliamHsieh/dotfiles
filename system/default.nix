b5caedd5089b6570a0a5638adfdddf8f5e0a0b2e
William Hsieh <williamhsieh@synology.com>
2024-01-31 19:01:26 +0800

    feat(nixos): almost default nixos configuration
    changes: format, font, input, packages, logitech options

---
+++ system/default.nix
@@ -0,0 +1,215 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      # Include the results of the hardware scan.
+      ./hardware.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Taipei";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "zh_TW.UTF-8";
+    LC_IDENTIFICATION = "zh_TW.UTF-8";
+    LC_MEASUREMENT = "zh_TW.UTF-8";
+    LC_MONETARY = "zh_TW.UTF-8";
+    LC_NAME = "zh_TW.UTF-8";
+    LC_NUMERIC = "zh_TW.UTF-8";
+    LC_PAPER = "zh_TW.UTF-8";
+    LC_TELEPHONE = "zh_TW.UTF-8";
+    LC_TIME = "zh_TW.UTF-8";
+  };
+
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs; [ fcitx5-chewing fcitx5-material-color fcitx5-rime ];
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.william = {
+    isNormalUser = true;
+    description = "William Hsieh";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      firefox
+      thunderbird
+      skypeforlinux
+    ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    kitty
+    alacritty
+    firefox
+    google-chrome
+    wget
+    logiops
+  ];
+
+  fonts.packages = with pkgs; [
+    (nerdfonts.override { fonts = [ "Iosevka" "JetBrainsMono" "CodeNewRoman" "Meslo" "FiraCode" "DroidSansMono" ]; })
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+  systemd.services.logiops = {
+    description = "An unofficial userspace driver for HID++ Logitech devices";
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.logiops}/bin/logid";
+    };
+  };
+
+  # NOTE: https://github.com/PixlOne/logiops/blob/main/logid.example.cfg
+  environment.etc."logid.cfg".text = ''
+    devices: ({
+      name: "Wireless Mouse MX Master 3";
+      smartshift: {
+        on: true;
+        threshold: 12;
+      };
+      hiresscroll: {
+        hires: true;
+        target: false;
+      };
+      dpi: 1200;
+      buttons: ({
+        cid: 0xc3;
+        action = {
+          type: "Gestures";
+          gestures: ({
+            direction: "Left";
+            mode: "OnRelease";
+            action = {
+              type = "Keypress";
+              keys: ["KEY_F15"];
+            };
+          }, {
+            direction: "Right";
+            mode: "OnRelease";
+            action = {
+              type = "Keypress";
+              keys: ["KEY_F16"];
+            };
+          }, {
+            direction: "Down";
+            mode: "OnRelease";
+            action = {
+              type: "Keypress";
+              keys: ["KEY_F17"];
+            };
+          }, {
+            direction: "Up";
+            mode: "OnRelease";
+            action = {
+              type: "Keypress";
+              keys: ["KEY_F18"];
+            };
+          }, {
+            direction: "None";
+            mode: "OnRelease";
+            action = {
+              type = "Keypress";
+              keys: ["KEY_PLAYPAUSE"];
+            };
+          });
+        };
+      }, {
+        cid: 0xc4;
+        action = {
+          type: "Keypress";
+          keys: ["KEY_F19"];
+        };
+      });
+    });
+  '';
+}
