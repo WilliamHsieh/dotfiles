a676591bf0fb8b9c366e912b9b76c61120e00f83
William Hsieh <wh31110@gmail.com>
2024-02-26 19:08:14 +0800

    feat(flatten): open buffer from `:term` in current nvim instance

--- config/nvim/lua/plugins/terminal.lua
+++ config/nvim/lua/plugins/terminal.lua
@@ -1,10 +1,7 @@
-local M = {
+local toggleterm = {
   "akinsho/toggleterm.nvim",
   cmd = { "ToggleTerm", "TermExec" },
-}
-
-function M.config()
-  require('toggleterm').setup {
+  opts = {
     open_mapping = [[<c-\>]],
     size = 20,
     hide_numbers = true,
@@ -38,7 +35,35 @@ function M.config()
         link = 'FloatBorder'
       },
     },
-  }
-end
+  },
+}
 
-return M
+local flatten = {
+  "willothy/flatten.nvim",
+  priority = 1010,
+  opts = {
+    callbacks = {
+      pre_open = function()
+        -- Close toggleterm when an external open request is received
+        require("toggleterm").toggle(0)
+      end,
+      post_open = function(bufnr, winnr, ft, is_blocking)
+        if not is_blocking then
+          -- If it's a normal file, then reopen the terminal, then switch back to the newly opened window
+          -- This gives the appearance of the window opening independently of the terminal
+          require("toggleterm").toggle(0)
+          vim.api.nvim_set_current_win(winnr)
+        end
+      end,
+      block_end = function()
+        -- After blocking ends (for a git commit, etc), reopen the terminal
+        require("toggleterm").toggle(0)
+      end,
+    },
+  },
+}
+
+return {
+  toggleterm,
+  flatten,
+}
