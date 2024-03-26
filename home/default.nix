284ebe55d514ad6b417afdefa2015c374c6336b7
William Hsieh <wh31110@gmail.com>
2024-03-26 19:55:44 +0800

    refactor(git): `git.nix` module

--- home/default.nix
+++ home/default.nix
@@ -9,6 +9,7 @@ in
     ./zsh.nix
     ./tmux.nix
     ./fzf.nix
+    ./git.nix
     inputs.nix-index-database.hmModules.nix-index
   ];
 
@@ -179,49 +180,6 @@ in
 
   programs.zoxide = {
     enable = true;
-  };
-
-  programs.git = {
-    enable = true;
-    userName = cfg.name;
-    userEmail = cfg.email;
-    aliases = {
-      undo = "reset HEAD@{1}";
-      lg = "log --pretty=format:'%C(red)%h %C(blue)<%an> %C(green)%cs (%cr)  %C(reset)%s %C(auto)%d' --abbrev-commit --graph";
-      pushf = "push --force-with-lease";
-    };
-    delta = {
-      enable = true;
-      options = {
-        true-color = "always";
-        syntax-theme = "base16-256";
-        line-numbers = true;
-        diff-so-fancy = true;
-      };
-    };
-    ignores = [
-      "*.swp"
-      ".DS_Store"
-    ];
-    extraConfig = {
-      init.defaultBranch = "master";
-      merge = {
-        tool = "vimdiff";
-        conflictstyle = "diff3";
-      };
-      push = {
-        autoSetupRemote = true;
-        useForceIfIncludes = true;
-      };
-      pull.rebase = true;
-      rebase = {
-        autoStash = true;
-        rebaseMerges = true;
-        autosquash = true;
-        updateRefs = true;
-      };
-      mergetool.prompt = "false";
-    };
   };
 
   programs.man.generateCaches = true;
---
+++ home/git.nix
@@ -0,0 +1,48 @@
+{ pkgs, config, ... }:
+let
+  cfg = import ./config.nix;
+in
+{
+  programs.git = {
+    enable = true;
+    userName = cfg.name;
+    userEmail = cfg.email;
+    aliases = {
+      undo = "reset HEAD@{1}";
+      lg = "log --pretty=format:'%C(red)%h %C(blue)<%an> %C(green)%cs (%cr)  %C(reset)%s %C(auto)%d' --abbrev-commit --graph";
+      pushf = "push --force-with-lease";
+    };
+    delta = {
+      enable = true;
+      options = {
+        true-color = "always";
+        syntax-theme = "base16-256";
+        line-numbers = true;
+        diff-so-fancy = true;
+      };
+    };
+    ignores = [
+      "*.swp"
+      ".DS_Store"
+    ];
+    extraConfig = {
+      init.defaultBranch = "master";
+      merge = {
+        tool = "vimdiff";
+        conflictstyle = "diff3";
+      };
+      push = {
+        autoSetupRemote = true;
+        useForceIfIncludes = true;
+      };
+      pull.rebase = true;
+      rebase = {
+        autoStash = true;
+        rebaseMerges = true;
+        autosquash = true;
+        updateRefs = true;
+      };
+      mergetool.prompt = "false";
+    };
+  };
+}
