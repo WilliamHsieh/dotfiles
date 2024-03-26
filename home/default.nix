b85e2bac273679d02d75841bbcd6aff331cbdce6
William Hsieh <wh31110@gmail.com>
2024-03-26 19:49:30 +0800

    feat(git): push --force-with-lease --force-if-includes
    ref:
    https://stackoverflow.com/questions/65837109/when-should-i-use-git-push-force-if-includes

--- home/default.nix
+++ home/default.nix
@@ -188,6 +188,7 @@ in
     aliases = {
       undo = "reset HEAD@{1}";
       lg = "log --pretty=format:'%C(red)%h %C(blue)<%an> %C(green)%cs (%cr)  %C(reset)%s %C(auto)%d' --abbrev-commit --graph";
+      pushf = "push --force-with-lease";
     };
     delta = {
       enable = true;
@@ -208,7 +209,10 @@ in
         tool = "vimdiff";
         conflictstyle = "diff3";
       };
-      push.autoSetupRemote = true;
+      push = {
+        autoSetupRemote = true;
+        useForceIfIncludes = true;
+      };
       pull.rebase = true;
       rebase = {
         autoStash = true;
