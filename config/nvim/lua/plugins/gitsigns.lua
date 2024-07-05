eba2d6f4042c1a5852c44deb279dc8ffd9e12cf0
William Hsieh <williamhsieh@synology.com>
2024-07-05 16:12:03 +0800

    fix(gitsigns): remove deprecated options (line blame relative_time)

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -3,25 +3,22 @@ local M = {
   cmd = "GitSigns",
 }
 
-function M.config()
-  require("gitsigns").setup {
-    signs = {
-      add = { text = "▎" },
-      change = { text = "▎" },
-    },
-    current_line_blame_opts = {
-      virt_text = true,
-      virt_text_pos = "right_align", -- 'eol' | 'overlay' | 'right_align'
-      delay = 0,
-      ignore_whitespace = false,
-    },
-    current_line_blame_formatter_opts = {
-      relative_time = false,
-    },
-    preview_config = {
-      border = "rounded",
-    },
-  }
-end
+M.opts = {
+  signs = {
+    add = { text = "▎" },
+    change = { text = "▎" },
+  },
+  signs_staged = {
+    add = { text = "▎" },
+    change = { text = "▎" },
+  },
+  current_line_blame_opts = {
+    virt_text_pos = "right_align", -- 'eol' | 'overlay' | 'right_align'
+    delay = 500,
+  },
+  preview_config = {
+    border = "rounded",
+  },
+}
 
 return M
