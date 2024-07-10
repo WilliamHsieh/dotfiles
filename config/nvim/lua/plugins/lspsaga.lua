880f808718d1165330c23c120b5445246da221cc
William Hsieh <wh31110@gmail.com>
2024-07-10 19:06:57 +0800

    fix(lspsaga): some mappings && definition window size

--- config/nvim/lua/plugins/lspsaga.lua
+++ config/nvim/lua/plugins/lspsaga.lua
@@ -1,23 +1,32 @@
 local M = {
-  'glepnir/lspsaga.nvim',
+  "glepnir/lspsaga.nvim",
   cmd = "Lspsaga",
 }
 
 M.opts = {
   scroll_preview = {
-    scroll_down = "<C-f>",
-    scroll_up = "<C-b>",
+    scroll_down = "<C-d>",
+    scroll_up = "<C-u>",
   },
   finder = {
     keys = {
       edit = "<CR>",
       vsplit = "v",
       split = "s",
-    }
+    },
+  },
+  definition = {
+    width = 0.8,
+    height = 0.6,
+  },
+  callhierarchy = {
+    keys = {
+      edit = "<CR>",
+    },
   },
   symbol_in_winbar = {
     enable = false,
-  }
+  },
 }
 
 return M
