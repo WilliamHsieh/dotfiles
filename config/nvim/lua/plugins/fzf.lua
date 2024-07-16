73c90f8927f97dab7c16a559899493214168e645
William Hsieh <wh31110@gmail.com>
2024-07-16 10:56:47 +0800

    feat(fzf): toggle hidden files
    note: set `[1]` to true to inherit from default config
    ref: https://github.com/ibhagwan/fzf-lua/commit/bd291dc51581a49bee50ea7a309fad7137f7e378

--- config/nvim/lua/plugins/fzf.lua
+++ config/nvim/lua/plugins/fzf.lua
@@ -7,22 +7,18 @@ local M = {
 M.opts = {
   "default-title",
   keymap = {
-    -- These override the default tables completely
-    -- no need to set to `false` to disable a bind
-    -- delete or modify is sufficient
     builtin = {
-      -- neovim `:tmap` mappings for the fzf win
-      ["<F1>"] = "toggle-help",
-      ["<F2>"] = "toggle-fullscreen",
-      -- Only valid with the 'builtin' previewer
-      ["<F3>"] = "toggle-preview-wrap",
-      ["<F4>"] = "toggle-preview",
-      -- Rotate preview clockwise/counter-clockwise
-      ["<F5>"] = "toggle-preview-ccw",
-      ["<F6>"] = "toggle-preview-cw",
+      true,
       ["<C-d>"] = "preview-page-down",
       ["<C-u>"] = "preview-page-up",
-      ["<C-h>"] = "preview-page-reset",
+    },
+  },
+  actions = {
+    files = {
+      true,
+      ["ctrl-h"] = function(...)
+        require("fzf-lua.actions").toggle_hidden(...)
+      end,
     },
   },
   previewers = {
