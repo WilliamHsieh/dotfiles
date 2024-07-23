61fa1713098971393e13c6f8b1d27e50aaca5134
William Hsieh <wh31110@gmail.com>
2024-07-23 17:16:43 +0800

    feat: copilotchat

---
+++ config/nvim/lua/plugins/copilotchat.lua
@@ -0,0 +1,55 @@
+local M = {
+  "CopilotC-Nvim/CopilotChat.nvim",
+  cmd = "CopilotChat",
+  build = function()
+    vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
+  end,
+}
+
+M.opts = {
+  -- NOTE: https://github.com/CopilotC-Nvim/CopilotChat.nvim/issues/366
+  model = "gpt-4-0125-preview",
+  prompts = {
+    Explain = {
+      prompt =
+      "/COPILOT_EXPLAIN Write an explanation for the active selection as paragraphs of text. If the context is code, include the relevent code snippet and corresponding line number at the beginning of each praragraph, separated by a line break.",
+    },
+  },
+}
+
+M.keys = {
+  {
+    "K",
+    "<cmd>CopilotChatExplain<cr>",
+    mode = "v",
+    desc = "CopilotChat - Explain code",
+  },
+  {
+    "<leader>ch",
+    "<cmd>CopilotChat<cr>",
+    desc = "CopilotChat - open chat history",
+  },
+  {
+    "<leader>cd",
+    "<cmd>CopilotChatFixDiagnostic<cr>",
+    desc = "CopilotChat - Fix diagnostic",
+  },
+  {
+    "<leader>co",
+    function()
+      local is_visual = vim.fn.mode():lower():find("v")
+      local select = require("CopilotChat.select")
+
+      local input = vim.fn.input("Quick Chat: ")
+      if input ~= "" then
+        require("CopilotChat").ask(input, {
+          selection = is_visual and select.visual or select.buffer,
+        })
+      end
+    end,
+    mode = { "n", "v" },
+    desc = "CopilotChat - Quick chat",
+  },
+}
+
+return M
