8aaca05728a1ead9f82d536ba479f0de742a7c96
William Hsieh <wh31110@gmail.com>
2024-08-01 15:54:29 +0800

    chore(nvim): rename core modules

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,6 +1,6 @@
-require "core.setting"
-require "core.plugins"
-require "core.autocmd"
+require("core.options")
+require("core.bootstrap")
+require("core.autocmd")
 
 vim.api.nvim_create_autocmd("User", {
   pattern = "VeryLazy",
