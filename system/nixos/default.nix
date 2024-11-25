7e93823c0afaa8e37d98fc8fe5f0702a14887c4f
William Hsieh <williamhsieh@synology.com>
2024-11-25 20:18:11 +0800

    remove options.sound(default to pipewire)

--- system/nixos/default.nix
+++ system/nixos/default.nix
@@ -73,8 +73,6 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
