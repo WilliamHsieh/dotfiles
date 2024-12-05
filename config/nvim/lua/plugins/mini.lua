e913c5fe73bfa58862cd724bc4ce5df780b69c13
William Hsieh <wh31110@gmail.com>
2024-12-05 16:55:02 +0800

    fix(mini.files): open the directory containing the current file

--- config/nvim/lua/plugins/mini.lua
+++ config/nvim/lua/plugins/mini.lua
@@ -6,15 +6,6 @@ return {
         "<leader>e",
         function()
           if not require("mini.files").close() then
-            require("mini.files").open()
-          end
-        end,
-        desc = "File Explorer: last used state",
-      },
-      {
-        "<leader>E",
-        function()
-          if not require("mini.files").close() then
             require("mini.files").open(vim.api.nvim_buf_get_name(0))
           end
         end,
