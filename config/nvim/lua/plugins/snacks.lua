364f80e4e776408da1bd420c9448924cefe55010
William Hsieh <wh31110@gmail.com>
2024-12-25 18:08:51 +0800

    feat(snacks): replace `vim-illuminate` with `words` module
    - still some bug, e.g. highlight not disabled when cursor leave a word

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -4,6 +4,10 @@ return {
 
   ---@type snacks.config
   opts = {
+    words = {
+      enabled = true,
+      debounce = 150,
+    },
     bigfile = { enabled = true },
     notifier = { enabled = true },
     quickfile = { enabled = true },
@@ -37,10 +41,30 @@ return {
   },
 
   init = function()
+    local snacks = require("snacks")
+
     vim.api.nvim_create_autocmd("User", {
       pattern = "MiniFilesActionRename",
       callback = function(event)
-        require("snacks").rename.on_rename_file(event.data.from, event.data.to)
+        snacks.rename.on_rename_file(event.data.from, event.data.to)
+      end,
+    })
+
+    local set_jump_words_keymap = function(buffer)
+      local opts = buffer and { buffer = buffer } or nil
+      vim.keymap.set({ "n", "t" }, "]]", function()
+        snacks.words.jump(vim.v.count1)
+      end, opts)
+      vim.keymap.set({ "n", "t" }, "[[", function()
+        snacks.words.jump(-vim.v.count1)
+      end, opts)
+    end
+
+    vim.schedule(set_jump_words_keymap)
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function()
+        local buffer = vim.api.nvim_get_current_buf()
+        set_jump_words_keymap(buffer)
       end,
     })
   end,
