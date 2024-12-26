3e8f17fa61878e01d8f06d7c33e786f03dfb537e
William Hsieh <wh31110@gmail.com>
2024-12-26 12:02:32 +0800

    feat(copilot): change default model, context selection, predefined actions

--- config/nvim/lua/plugins/copilotchat.lua
+++ config/nvim/lua/plugins/copilotchat.lua
@@ -2,22 +2,24 @@
 local M = {
   "CopilotC-Nvim/CopilotChat.nvim",
   cmd = "CopilotChat",
-  build = function()
-    vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
-  end,
 }
 
 ---@type CopilotChat.config
 M.opts = {
-  insert_at_end = true,
+  model = "claude-3.5-sonnet",
   auto_follow_cursor = false,
   highlight_headers = false,
   separator = "",
   error_header = "> [!ERROR] Error",
   prompts = {
     Explain = {
-      prompt =
-      "/COPILOT_EXPLAIN Write an explanation for the active selection as paragraphs of text. If the context is code, include the relevent code snippet and corresponding line number at the beginning of each praragraph, separated by a line break.",
+      prompt = "/COPILOT_EXPLAIN Write an explanation for the active selection as paragraphs of text. If the context is code, include the relevent code snippet and corresponding line number at the beginning of each praragraph, separated by a line break.",
+    },
+  },
+  mappings = {
+    complete = {
+      -- otherwise it's conflicting with cmp.nvim
+      insert = "<S-Tab>",
     },
   },
 }
@@ -38,6 +40,15 @@ M.keys = {
     "<leader>cd",
     "<cmd>CopilotChatFixDiagnostic<cr>",
     desc = "CopilotChat - Fix diagnostic",
+  },
+  {
+    "<leader>ca",
+    function()
+      local actions = require("CopilotChat.actions")
+      require("CopilotChat.integrations.fzflua").pick(actions.prompt_actions())
+    end,
+    mode = { "n", "v" },
+    desc = "CopilotChat - Prompt actions",
   },
   {
     "<leader>co",
