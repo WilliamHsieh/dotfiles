401758042fc8fa23dc0d6a40e9082da2665b7622
William Hsieh <william.hsieh@kronosresearch.com>
2025-03-31 11:00:40 +0800

    feat(nvim): version 0.11

--- config/nvim/lua/plugins/terminal.lua
+++ config/nvim/lua/plugins/terminal.lua
@@ -42,23 +42,18 @@ local flatten = {
   "willothy/flatten.nvim",
   priority = 1010,
   opts = {
-    callbacks = {
+    hooks = {
       pre_open = function()
         -- Close toggleterm when an external open request is received
         require("toggleterm").toggle(0)
-      end,
-      post_open = function(bufnr, winnr, ft, is_blocking)
-        if not is_blocking then
-          -- If it's a normal file, then reopen the terminal, then switch back to the newly opened window
-          -- This gives the appearance of the window opening independently of the terminal
-          require("toggleterm").toggle(0)
-          vim.api.nvim_set_current_win(winnr)
-        end
       end,
       block_end = function()
         -- After blocking ends (for a git commit, etc), reopen the terminal
         require("toggleterm").toggle(0)
       end,
+    },
+    window = {
+      open = "vsplit",
     },
   },
 }
