1984998372c1e18b7165bcbfd8ca8fc545eda1f7
William Hsieh <william.hsieh@kronosresearch.com>
2025-06-04 06:24:45 +0000

    feat(mini.files): mapping for hidden files and splits

--- config/nvim/lua/plugins/mini.lua
+++ config/nvim/lua/plugins/mini.lua
@@ -66,5 +66,59 @@ return {
         width_preview = 35,
       },
     },
+    config = function(_, opts)
+      require("mini.files").setup(opts)
+
+      -- open in split
+      local map_split = function(buf_id, lhs, direction)
+        vim.keymap.set("n", lhs, function()
+          local cur_target = MiniFiles.get_explorer_state().target_window
+
+          -- Make new window and set it as target
+          local new_target = vim.api.nvim_win_call(cur_target, function()
+            vim.cmd(direction .. " split")
+            return vim.api.nvim_get_current_win()
+          end)
+
+          MiniFiles.set_target_window(new_target)
+          MiniFiles.go_in { close_on_file = true }
+        end, { buffer = buf_id, desc = "Split " .. direction })
+      end
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "MiniFilesBufferCreate",
+        callback = function(args)
+          local buf_id = args.data.buf_id
+          map_split(buf_id, "<C-s>", "belowright horizontal")
+          map_split(buf_id, "<C-v>", "belowright vertical")
+        end,
+      })
+
+      -- toggle hidden files
+      local show_hidden_files = true
+
+      local filter_show = function()
+        return true
+      end
+
+      local filter_hide = function(fs_entry)
+        return not vim.startswith(fs_entry.name, ".")
+      end
+
+      local toggle_dotfiles = function()
+        show_hidden_files = not show_hidden_files
+        local new_filter = show_hidden_files and filter_show or filter_hide
+        vim.notify("Hidden files are " .. (show_hidden_files and "shown" or "hidden"))
+        MiniFiles.refresh { content = { filter = new_filter } }
+      end
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "MiniFilesBufferCreate",
+        callback = function(args)
+          local buf_id = args.data.buf_id
+          vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf_id })
+        end,
+      })
+    end
   },
 }
