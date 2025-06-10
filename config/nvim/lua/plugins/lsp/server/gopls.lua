792e284f05bbe1411a83ccdaa4641bec97ff69d0
witherslin <witherslin@synology.com>
2025-06-10 05:06:35 +0000

    feat(go): special lsp config

---
+++ config/nvim/lua/plugins/lsp/server/gopls.lua
@@ -0,0 +1,21 @@
+return {
+  settings = {
+    gopls = {
+      gofumpt = true,
+      hints = {
+        assignVariableTypes = true,
+        compositeLiteralFields = true,
+        compositeLiteralTypes = true,
+        constantValues = true,
+        functionTypeParameters = true,
+        parameterNames = true,
+        rangeVariableTypes = true,
+      },
+      usePlaceholders = true,
+      completeUnimported = true,
+      staticcheck = true,
+      semanticTokens = true,
+      directoryFilters = { "-.git" },
+    },
+  },
+}
