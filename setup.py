54228648690a975a92d843ce64ba68f0f2146bd1
WilliamHsieh <wh31110@gmail.com>
2025-06-12 22:22:18 +0800

    fix(script): nixos bootstrap (#105)

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
+import subprocess
+import os
+
 
 def command_output(command):
-    import subprocess
-
-    return subprocess.check_output(command, shell=True).decode().strip().strip('"')
+    env = os.environ.copy()
+    env["NIX_CONFIG"] = "experimental-features = flakes nix-command"
+    return subprocess.check_output(command, shell=True, env=env).decode().strip()
 
 
 def parse_args():
@@ -75,8 +78,18 @@ def parse_args():
 
 
 def write_config(args):
+    if args.profile == "nixos":
+        hardware_config = command_output(
+            "nixos-generate-config --show-hardware-config || true"
+        )
+        if hardware_config != "":
+            with open(args.dir + "/system/nixos/hardware.nix", "w") as f:
+                f.write(hardware_config)
+
     config = {
-        "system": command_output("nix eval --impure --expr 'builtins.currentSystem'"),
+        "system": command_output(
+            "nix eval --impure --expr 'builtins.currentSystem'"
+        ).strip('"'),
         "profile": args.profile,
         "directory": args.dir,
         "username": args.username,
@@ -92,7 +105,7 @@ def write_config(args):
 
 def get_command():
     args = parse_args()
-    if args.bootstrap:
+    if args.bootstrap and not args.dry:
         write_config(args)
 
     def get_derivation():
@@ -118,8 +131,11 @@ def get_command():
 
     cmd += args.remainder
 
-    if not args.build and args.profile == "home":
-        cmd += ["-b", "backup"]
+    if not args.build:
+        if args.profile == "home":
+            cmd += ["-b", "backup"]
+        else:
+            cmd = ["sudo"] + cmd
 
     if args.dry:
         cmd = ["echo"] + cmd
@@ -130,8 +146,6 @@ def get_command():
 
 
 def main():
-    import os
-
     cmd = get_command()
     os.execvp(cmd[0], cmd)
 
