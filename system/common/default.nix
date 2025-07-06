34bef3463dd8187fde761a4c7da7d5f31a634d4f
William Hsieh <wh31110@gmail.com>
2025-07-07 00:29:01 +0800

    feat(nix): use user packages && backup extension
    - `useUserPackages` means nixos user pkgs and home-manager pkgs will be
      accumulated in `/etc/profiles/per-user/USER`
    - `.nix-profile` no longer exists

--- system/common/default.nix
+++ system/common/default.nix
@@ -4,12 +4,14 @@
     users.${dotfiles.username} = import ../../home;
 
     useGlobalPkgs = true;
-    # useUserPackages = true;
+    useUserPackages = true;
 
     extraSpecialArgs = {
       inherit inputs dotfiles;
       isSystemConfig = true;
     };
+
+    backupFileExtension = "nix-generated-backup";
   };
 
   nixpkgs.hostPlatform = dotfiles.system;
