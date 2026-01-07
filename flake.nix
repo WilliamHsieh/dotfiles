2aa6ad3baeec9c70e2066d047f1d98f446044467
William Hsieh <william.hsieh@kronosresearch.com>
2026-01-07 02:37:06 +0000

    chore(nix): follow catppuccin release branch

--- flake.nix
+++ flake.nix
@@ -31,8 +31,7 @@
     };
 
     catppuccin = {
-      url = "github:catppuccin/nix";
-      # url = "github:catppuccin/nix/release-25.11";
+      url = "github:catppuccin/nix/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
