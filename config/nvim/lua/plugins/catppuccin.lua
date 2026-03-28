677c893bfd4c177686d84b00c03b35c31f29929c
William Hsieh <wh31110@gmail.com>
2026-03-28 16:03:11 +0800

    keep most catppuccin colors same as before

--- config/nvim/lua/plugins/catppuccin.lua
+++ config/nvim/lua/plugins/catppuccin.lua
@@ -7,12 +7,50 @@ local M = {
 function M.config()
   require("catppuccin").setup {
     custom_highlights = function(colors)
+      local options = require("catppuccin").options
+
       return {
         NormalFloat = { bg = colors.base },
         ExtraWhitespace = { fg = colors.yellow, style = { "underline" } },
         -- HACK: https://github.com/neovim/neovim/pull/25941
         StatusLine = { fg = colors.crust, bg = "none" },
         StatusLineNC = { fg = colors.mantle, bg = "none" },
+
+        -- HACK: version 2.0 color changes
+        -- https://github.com/catppuccin/nvim/pull/804#pullrequestreview-3080755868
+        ["@variable.member"] = { fg = colors.lavender }, -- For fields.
+        ["@module"] = { fg = colors.lavender, style = options.styles.miscs or { "italic" } }, -- For identifiers referring to modules and namespaces.
+        ["@string.special.url"] = { fg = colors.rosewater, style = { "italic", "underline" } }, -- urls, links and emails
+        ["@type.builtin"] = { fg = colors.yellow, style = options.styles.properties or { "italic" } }, -- For builtin types.
+        ["@property"] = { fg = colors.lavender, style = options.styles.properties or {} }, -- Same as TSField.
+        ["@constructor"] = { fg = colors.sapphire }, -- For constructor calls and definitions: = { } in Lua, and Java constructors.
+        ["@keyword.operator"] = { link = "Operator" }, -- For new keyword operator
+        ["@keyword.export"] = { fg = colors.sky, style = options.styles.keywords },
+        ["@markup.strong"] = { fg = colors.maroon, style = { "bold" } }, -- bold
+        ["@markup.italic"] = { fg = colors.maroon, style = { "italic" } }, -- italic
+        ["@markup.heading"] = { fg = colors.blue, style = { "bold" } }, -- titles like: # Example
+        ["@markup.quote"] = { fg = colors.maroon, style = { "bold" } }, -- block quotes
+        ["@markup.link"] = { link = "Tag" }, -- text references, footnotes, citations, etc.
+        ["@markup.link.label"] = { link = "Label" }, -- link, reference descriptions
+        ["@markup.link.url"] = { fg = colors.rosewater, style = { "italic", "underline" } }, -- urls, links and emails
+        ["@markup.raw"] = { fg = colors.teal }, -- used for inline code in markdown and for doc in python (""")
+        ["@markup.list"] = { link = "Special" },
+        ["@tag"] = { fg = colors.mauve }, -- Tags like html tag names.
+        ["@tag.attribute"] = { fg = colors.teal, style = options.styles.miscs or { "italic" } }, -- Tags like html tag names.
+        ["@tag.delimiter"] = { fg = colors.sky }, -- Tag delimiter like < > /
+        ["@property.css"] = { fg = colors.lavender },
+        ["@property.id.css"] = { fg = colors.blue },
+        ["@type.tag.css"] = { fg = colors.mauve },
+        ["@string.plain.css"] = { fg = colors.peach },
+        ["@constructor.lua"] = { fg = colors.flamingo }, -- For constructor calls and definitions: = { } in Lua.
+        -- typescript
+        ["@property.typescript"] = { fg = colors.lavender, style = options.styles.properties or {} },
+        ["@constructor.typescript"] = { fg = colors.lavender },
+        -- TSX (Typescript React)
+        ["@constructor.tsx"] = { fg = colors.lavender },
+        ["@tag.attribute.tsx"] = { fg = colors.teal, style = options.styles.miscs or { "italic" } },
+        ["@type.builtin.c"] = { fg = colors.yellow, style = {} },
+        ["@type.builtin.cpp"] = { fg = colors.yellow, style = {} },
       }
     end,
     integrations = {
