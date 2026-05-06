0527e4e7ef18ac8d9810f7d29c5827355e691018
William Hsieh <wh31110@gmail.com>
2026-05-06 14:21:48 +0800

    fix(nvim): remove lazyfile hack and provide correct lsp init event

--- config/nvim/lua/core/utils.lua
+++ config/nvim/lua/core/utils.lua
@@ -89,31 +89,6 @@ function M.signal_handler(signum, callback)
 end
 
 function M.lazy_file()
-  -- This autocmd will only trigger when a file was loaded from the cmdline.
-  -- It will render the file as quickly as possible.
-  vim.api.nvim_create_autocmd("BufReadPost", {
-    once = true,
-    callback = function(event)
-      -- Skip if we already entered vim
-      if vim.v.vim_did_enter == 1 then
-        return
-      end
-
-      -- Try to guess the filetype (may change later on during Neovim startup)
-      local ft = vim.filetype.match { buf = event.buf }
-      if ft then
-        -- Add treesitter highlights and fallback to syntax
-        local lang = vim.treesitter.language.get_lang(ft)
-        if not (lang and pcall(vim.treesitter.start, event.buf, lang)) then
-          vim.bo[event.buf].syntax = ft
-        end
-
-        -- Trigger early redraw
-        vim.cmd.redraw()
-      end
-    end,
-  })
-
   local lazy_file_events = { "BufReadPost", "BufNewFile", "BufWritePre" }
 
   local Event = require("lazy.core.handler.event")
